ac5258255ffa89d1d0c8019f4849aae99f5aa04b
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-06-27 22:00:39 +0700

    feat: adding route paths

---
+++ app/dashboard/verification/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Center, Box, Text, HStack } from "@chakra-ui/react";
+
+export default function Verification() {
+  return (
+    <Center w={"100%"} h={"100vh"}>
+      <Box w={"25em"} h={"auto"} boxShadow={"lg"}>
+        <Box w={"full"} p={"2em"} bg={"RGBA(0, 0, 0, 0.92)"} rounded={"md"}>
+          <HStack spacing={3}>
+            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+          </HStack>
+          <Box mt={"1em"}>
+            <Text align={"left"} color={"white"} fontSize={"xl"} fontWeight={"normal"}>
+              Ini Verification
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </Center>
+  );
+}
