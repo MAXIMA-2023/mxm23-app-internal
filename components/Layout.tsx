9e5e496e4ed19dc264527b187e2a24c774bf1c9f
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-07-02 10:40:47 +0700

    chore(layout): changing background and adding sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Center, Box, Text, HStack } from "@chakra-ui/react";
+import Sidebar from "./Sidebar";
 
-function Layout() {
-  return <div>Layout</div>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Sidebar />
+      <Box w={"full"} h={"full"} bgColor={"#FAFAFA"}>
+        {children}
+      </Box>
+    </>
+  );
 }
-
-export default Layout;
