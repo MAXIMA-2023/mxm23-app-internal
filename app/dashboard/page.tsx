fba3d9c7d68d75521720e9351e7b54b53b852f41
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-07-02 10:41:35 +0700

    chore(dashboard): adding layout and sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,30 @@
 "use client";
+import Layout from "@/components/Layout";
+import Sidebar from "@/components/Sidebar";
 import { Center, Box, Text, HStack } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
-    <Center w={"100%"} h={"100vh"}>
-      <Box w={"25em"} h={"auto"} boxShadow={"lg"}>
-        <Box w={"full"} p={"2em"} bg={"RGBA(0, 0, 0, 0.92)"} rounded={"md"}>
-          <HStack spacing={3}>
-            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
-            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
-            <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
-          </HStack>
-          <Box mt={"1em"}>
-            <Text align={"left"} color={"white"} fontSize={"xl"} fontWeight={"normal"}>
-              Ini Dashboard
-            </Text>
+    <>
+      <title>MAXIMA 2023 Internal - Dashboard</title>
+      <Layout>
+        <Center w={"100%"} h={"100vh"}>
+          <Box w={"25em"} h={"auto"} boxShadow={"lg"}>
+            <Box w={"full"} p={"2em"} bg={"RGBA(0, 0, 0, 0.92)"} rounded={"md"}>
+              <HStack spacing={3}>
+                <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+                <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+                <Box boxSize={"1em"} bg={"white"} rounded={"full"}></Box>
+              </HStack>
+              <Box mt={"1em"}>
+                <Text align={"left"} color={"white"} fontSize={"xl"} fontWeight={"normal"}>
+                  Hi, selamat liburan para frontend MAXIMA 2023
+                </Text>
+              </Box>
+            </Box>
           </Box>
-        </Box>
-      </Box>
-    </Center>
+        </Center>
+      </Layout>
+    </>
   );
 }
