87509da0c79d198c048d1086b6adef51a356355e
rdhwan <fathanridhwann@gmail.com>
2023-07-05 00:11:11 +0700

    fix(qrscan): using layout for state and malpun

--- app/dashboard/qrscan/malpun/page.tsx
+++ app/dashboard/qrscan/malpun/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import {
-  Center,
   Box,
   Text,
   Modal,
@@ -14,17 +13,13 @@ import {
   Button,
   useDisclosure,
   Spacer,
-  Flex,
-  Icon,
 } from "@chakra-ui/react";
-import { BsArrowLeftShort } from "react-icons/bs";
+import Layout from "@/components/Layout";
 import QRScanner from "@/components/qrscan/QRScanner";
 import Swal from "sweetalert2";
 
 // !CHANGEME: Ini cuma dummy data, nanti diganti schema dengan POST requests
 import { DummyPresensiData } from "../data";
-import Layout from "@/components/Layout";
-import Link from "next/link";
 
 type Modal = {
   isSuccess: boolean;
@@ -40,156 +35,115 @@ export default function QRScanSTATE() {
   return (
     <>
       <title>MAXIMA 2023 Internal - QR Scan Malpun</title>
-      <Layout>
+      <Layout title="QR Scan MALPUN" showDashboardButton disablePadding>
         <Box
-          w={"full"}
-          py={["1em", "1em", "1.5em", "3em", "3em"]}
-          px={["1em", "1em", "1.5em", "2em", "2em"]}
+          w="full"
+          h="100vh"
+          position="relative"
+          borderRadius="2xl"
+          overflow="hidden"
         >
-          <Box w={"full"} pl={["0", "0", "0", "18em", "20em"]}>
-            <Box my={"2em"}>
-              <Link href={"/dashboard"}>
-                <Flex
-                  alignItems={"center"}
-                  textColor="#9CA3AF"
-                  _hover={{ textColor: "#185C99" }}
-                  cursor={"pointer"}
-                  transition={"0.1s ease-in-out"}
+          <QRScanner
+            onSuccess={(id) => {
+              // !CHANGEME: ganti dummy data dengan get init data dari get requests
+
+              // *debouncing, biar ga open berkali kali
+              if (isOpen) {
+                return;
+              }
+
+              // !TODO: Handling buat akun yang gak eligible atau belum bayar.
+              if (!DummyPresensiData.malpun.isEligible) {
+                Swal.fire(
+                  "Error!",
+                  DummyPresensiData.isInternal
+                    ? "Mahasiswa tidak eligible untuk mengikuti MALPUN"
+                    : "Peserta tidak berhak untuk mengikuti MALPUN karena belum bayar",
+                  "error"
+                );
+                return;
+              }
+
+              setModalState({
+                isSuccess: true,
+                id: id,
+              });
+              onOpen();
+            }}
+            onError={(reason) => {
+              setModalState({
+                isSuccess: false,
+                errorReason: reason,
+              });
+              onOpen();
+            }}
+          />
+
+          {/* !TODO: Buat modal alert */}
+          {/* ?TESTING: useState bikin re render gak ya?, sayang performance camera */}
+          <Modal isOpen={isOpen} onClose={onClose} isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader textColor="#1e1d22">
+                {modalState.isSuccess ? "Sukses!" : "Error!"}
+              </ModalHeader>
+              <ModalCloseButton />
+
+              <ModalBody textColor="#1e1d22">
+                {modalState.isSuccess ? (
+                  <Box>
+                    {DummyPresensiData.isInternal ? (
+                      <Text>
+                        <Text fontWeight={"bold"}>NIM</Text>{" "}
+                        {DummyPresensiData.nim}
+                      </Text>
+                    ) : (
+                      <Text>
+                        <Text fontWeight={"bold"}>KTP</Text>{" "}
+                        {DummyPresensiData.ktp}
+                      </Text>
+                    )}
+                    <Text>
+                      <Text fontWeight={"bold"}>Nama</Text>{" "}
+                      {DummyPresensiData.name}
+                    </Text>
+                    <Text>
+                      <Text fontWeight={"bold"}>Status</Text>
+                      {DummyPresensiData.isInternal ? "Internal" : "External"}
+                    </Text>
+                  </Box>
+                ) : (
+                  <Text>{modalState.errorReason}</Text>
+                )}
+              </ModalBody>
+
+              <ModalFooter>
+                {modalState.isSuccess && (
+                  <Button
+                    color="#185C99"
+                    mr={3}
+                    onClick={() => {
+                      // !CHANGEME: POST Requests absen masuk dan schema buat disable button
+                      console.log("ABSEN MASUK MALPUN");
+                      onClose();
+                    }}
+                    isDisabled={DummyPresensiData.malpun.isHadir}
+                  >
+                    Masuk
+                  </Button>
+                )}
+                <Spacer />
+                <Button
+                  color="#185C99"
+                  variant={"outline"}
+                  mr={3}
+                  onClick={onClose}
                 >
-                  <Icon as={BsArrowLeftShort} boxSize={6} />
-                  <Text ml={"0.7em"} fontSize={"md"}>
-                    Back to Dashboard
-                  </Text>
-                </Flex>
-              </Link>
-              <Text
-                fontSize={"4xl"}
-                fontWeight={"medium"}
-                textColor={"#1E1D22"}
-              >
-                QR Scan MALPUN
-              </Text>
-            </Box>
-            <Flex
-              w={"full"}
-              minH={["95vh", "95vh", "auto", "86vh", "86vh"]}
-              bgColor={"white"}
-              borderRadius={"2xl"}
-              boxShadow={"lg"}
-            >
-              <Box
-                w="full"
-                position="relative"
-                borderRadius="2xl"
-                overflow="hidden"
-              >
-                <QRScanner
-                  onSuccess={(id) => {
-                    // !CHANGEME: ganti dummy data dengan get init data dari get requests
-
-                    // *debouncing, biar ga open berkali kali
-                    if (isOpen) {
-                      return;
-                    }
-
-                    if (!DummyPresensiData.state.isEligible) {
-                      Swal.fire(
-                        "Error!",
-                        "Mahasiswa tidak Eligible untuk mengikuti STATE",
-                        "error"
-                      );
-                      return;
-                    }
-
-                    setModalState({
-                      isSuccess: true,
-                      id: id,
-                    });
-                    onOpen();
-                  }}
-                  onError={(reason) => {
-                    setModalState({
-                      isSuccess: false,
-                      errorReason: reason,
-                    });
-                    onOpen();
-                  }}
-                />
-
-                {/* !TODO: Buat modal alert */}
-                {/* ?TESTING: useState bikin re render gak ya?, sayang performance camera */}
-                <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader textColor="#1e1d22">
-                      {modalState.isSuccess ? "Sukses!" : "Error!"}
-                    </ModalHeader>
-                    <ModalCloseButton />
-
-                    <ModalBody textColor="#1e1d22">
-                      {modalState.isSuccess ? (
-                        <Box>
-                          {DummyPresensiData.isInternal ? (
-                            <Text>
-                              <Text fontWeight={"bold"}>NIM</Text>{" "}
-                              {DummyPresensiData.nim}
-                            </Text>
-                          ) : (
-                            <Text>
-                              <Text fontWeight={"bold"}>KTP</Text>{" "}
-                              {DummyPresensiData.ktp}
-                            </Text>
-                          )}
-                          <Text>
-                            <Text fontWeight={"bold"}>Nama</Text>{" "}
-                            {DummyPresensiData.name}
-                          </Text>
-                          <Text>
-                            <Text fontWeight={"bold"}>Status</Text>
-                            {DummyPresensiData.isInternal
-                              ? "Internal"
-                              : "External"}
-                          </Text>
-                        </Box>
-                      ) : (
-                        <Text>{modalState.errorReason}</Text>
-                      )}
-                    </ModalBody>
-
-                    <ModalFooter>
-                      {modalState.isSuccess && (
-                        <Button
-                          color="#185C99"
-                          mr={3}
-                          onClick={() => {
-                            // !CHANGEME: POST Requests absen masuk dan schema buat disable button
-                            console.log("ABSEN MASUK MALPUN");
-                            onClose();
-                          }}
-                          isDisabled={DummyPresensiData.malpun.isHadir}
-                        >
-                          Masuk
-                        </Button>
-                      )}
-                      <Spacer />
-                      <Button
-                        color="#185C99"
-                        variant={"outline"}
-                        mr={3}
-                        onClick={onClose}
-                      >
-                        Cancel
-                      </Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-                <Box
-                  maxH={["88.5%", "60vh", "75vh", "70vh", "70vh"]}
-                  overflowY={"auto"}
-                ></Box>
-              </Box>
-            </Flex>
-          </Box>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </Box>
       </Layout>
     </>
--- app/dashboard/qrscan/state/page.tsx
+++ app/dashboard/qrscan/state/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import {
-  Center,
   Box,
   Text,
   Modal,
@@ -14,17 +13,13 @@ import {
   Button,
   useDisclosure,
   Spacer,
-  Flex,
-  Icon,
 } from "@chakra-ui/react";
-import { BsArrowLeftShort } from "react-icons/bs";
+import Layout from "@/components/Layout";
 import QRScanner from "@/components/qrscan/QRScanner";
 import Swal from "sweetalert2";
 
 // !CHANGEME: Ini cuma dummy data, nanti diganti schema dengan POST requests
 import { DummyPresensiData } from "../data";
-import Layout from "@/components/Layout";
-import Link from "next/link";
 
 type Modal = {
   isSuccess: boolean;
@@ -40,173 +35,127 @@ export default function QRScanSTATE() {
   return (
     <>
       <title>MAXIMA 2023 Internal - QR Scan State</title>
-      <Layout>
+      <Layout title="QR Scan STATE" showDashboardButton disablePadding>
         <Box
-          w={"full"}
-          py={["1em", "1em", "1.5em", "3em", "3em"]}
-          px={["1em", "1em", "1.5em", "2em", "2em"]}
+          w="full"
+          h="100vh"
+          position="relative"
+          borderRadius="2xl"
+          overflow="hidden"
         >
-          <Box w={"full"} pl={["0", "0", "0", "18em", "20em"]}>
-            <Box my={"2em"}>
-              <Link href={"/dashboard"}>
-                <Flex
-                  alignItems={"center"}
-                  textColor="#9CA3AF"
-                  _hover={{ textColor: "#185C99" }}
-                  cursor={"pointer"}
-                  transition={"0.1s ease-in-out"}
-                >
-                  <Icon as={BsArrowLeftShort} boxSize={6} />
-                  <Text ml={"0.7em"} fontSize={"md"}>
-                    Back to Dashboard
-                  </Text>
-                </Flex>
-              </Link>
-              <Text
-                fontSize={"4xl"}
-                fontWeight={"medium"}
-                textColor={"#1E1D22"}
-              >
-                QR Scan STATE
-              </Text>
-            </Box>
-            <Flex
-              w={"full"}
-              minH={["95vh", "95vh", "auto", "86vh", "86vh"]}
-              bgColor={"white"}
-              borderRadius={"2xl"}
-              boxShadow={"lg"}
-            >
-              <Box
-                w="full"
-                position="relative"
-                borderRadius="2xl"
-                overflow="hidden"
-              >
-                <QRScanner
-                  onSuccess={(id) => {
-                    // !CHANGEME: ganti dummy data dengan get init data dari get requests
+          <QRScanner
+            onSuccess={(id) => {
+              // !CHANGEME: ganti dummy data dengan get init data dari get requests
 
-                    // *debouncing, biar ga open berkali kali
-                    if (isOpen) {
-                      return;
-                    }
+              // *debouncing, biar ga open berkali kali
+              if (isOpen) {
+                return;
+              }
 
-                    if (!DummyPresensiData.state.isEligible) {
-                      Swal.fire(
-                        "Error!",
-                        "Mahasiswa tidak Eligible untuk mengikuti STATE",
-                        "error"
-                      );
-                      return;
-                    }
+              if (!DummyPresensiData.state.isEligible) {
+                Swal.fire(
+                  "Error!",
+                  "Mahasiswa tidak Eligible untuk mengikuti STATE",
+                  "error"
+                );
+                return;
+              }
 
-                    setModalState({
-                      isSuccess: true,
-                      id: id,
-                    });
-                    onOpen();
-                  }}
-                  onError={(reason) => {
-                    setModalState({
-                      isSuccess: false,
-                      errorReason: reason,
-                    });
-                    onOpen();
-                  }}
-                />
+              setModalState({
+                isSuccess: true,
+                id: id,
+              });
+              onOpen();
+            }}
+            onError={(reason) => {
+              setModalState({
+                isSuccess: false,
+                errorReason: reason,
+              });
+              onOpen();
+            }}
+          />
 
-                {/* !TODO: Buat modal alert */}
-                {/* ?TESTING: useState bikin re render gak ya?, sayang performance camera */}
-                <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader textColor="#1e1d22" fontWeight="bold">
-                      {modalState.isSuccess ? "Sukses!" : "Error!"}
-                    </ModalHeader>
-                    <ModalCloseButton />
+          {/* !TODO: Buat modal alert */}
+          {/* ?TESTING: useState bikin re render gak ya?, sayang performance camera */}
+          <Modal isOpen={isOpen} onClose={onClose} isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader textColor="#1e1d22" fontWeight="bold">
+                {modalState.isSuccess ? "Sukses!" : "Error!"}
+              </ModalHeader>
+              <ModalCloseButton />
 
-                    <ModalBody textColor="#1e1d22">
-                      {modalState.isSuccess ? (
-                        <Box>
-                          <Text>
-                            <Text fontWeight={"bold"}>NIM</Text>{" "}
-                            {DummyPresensiData.nim}
-                          </Text>
-                          <Text>
-                            <Text fontWeight={"bold"}>Nama</Text>{" "}
-                            {DummyPresensiData.name}
-                          </Text>
-                          <Text>
-                            <Text fontWeight={"bold"}>State</Text>
-                            {DummyPresensiData.state.pilihan}
-                          </Text>
-                          {DummyPresensiData.state.masuk.isHadir && (
-                            <Text>
-                              <Text fontWeight={"bold"}>Presensi Masuk</Text>
-                              {DummyPresensiData.state.masuk.presensiAt}
-                            </Text>
-                          )}
-                        </Box>
-                      ) : (
-                        <Text>{modalState.errorReason}</Text>
-                      )}
-                    </ModalBody>
+              <ModalBody textColor="#1e1d22">
+                {modalState.isSuccess ? (
+                  <Box>
+                    <Text>
+                      <Text fontWeight={"bold"}>NIM</Text>{" "}
+                      {DummyPresensiData.nim}
+                    </Text>
+                    <Text>
+                      <Text fontWeight={"bold"}>Nama</Text>{" "}
+                      {DummyPresensiData.name}
+                    </Text>
+                    <Text>
+                      <Text fontWeight={"bold"}>State</Text>
+                      {DummyPresensiData.state.pilihan}
+                    </Text>
+                    {DummyPresensiData.state.masuk.isHadir && (
+                      <Text>
+                        <Text fontWeight={"bold"}>Presensi Masuk</Text>
+                        {DummyPresensiData.state.masuk.presensiAt}
+                      </Text>
+                    )}
+                  </Box>
+                ) : (
+                  <Text>{modalState.errorReason}</Text>
+                )}
+              </ModalBody>
 
-                    <ModalFooter>
-                      {modalState.isSuccess && (
-                        <>
-                          <Button
-                            color="#185C99"
-                            mr={3}
-                            onClick={() => {
-                              // !CHANGEME: POST Requests absen masuk dan schema buat disable button
-                              console.log("ABSEN MASUK");
-                              onClose();
-                            }}
-                            isDisabled={DummyPresensiData.state.masuk.isHadir}
-                          >
-                            Masuk
-                          </Button>
-                          <Button
-                            color="#185C99"
-                            mr={3}
-                            onClick={() => {
-                              if (!DummyPresensiData.state.masuk.isHadir) {
-                                Swal.fire(
-                                  "Error!",
-                                  "Peserta belum absen masuk",
-                                  "warning"
-                                );
-                              }
-                              // !CHANGEME: POST Requests absen keluar dan schema buat disable button
-                              console.log("ABSEN KELUAR");
-                              onClose();
-                            }}
-                            isDisabled={DummyPresensiData.state.keluar.isHadir}
-                          >
-                            Keluar
-                          </Button>
-                        </>
-                      )}
-                      <Spacer />
-                      <Button
-                        color="#185C99"
-                        variant="outline"
-                        onClick={onClose}
-                      >
-                        Cancel
-                      </Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-                <Box
-                  maxH={["88.5%", "60vh", "75vh", "70vh", "70vh"]}
-                  overflowY={"auto"}
-                ></Box>
-              </Box>
-            </Flex>
-          </Box>
+              <ModalFooter>
+                {modalState.isSuccess && (
+                  <>
+                    <Button
+                      color="#185C99"
+                      mr={3}
+                      onClick={() => {
+                        // !CHANGEME: POST Requests absen masuk dan schema buat disable button
+                        console.log("ABSEN MASUK");
+                        onClose();
+                      }}
+                      isDisabled={DummyPresensiData.state.masuk.isHadir}
+                    >
+                      Masuk
+                    </Button>
+                    <Button
+                      color="#185C99"
+                      mr={3}
+                      onClick={() => {
+                        if (!DummyPresensiData.state.masuk.isHadir) {
+                          Swal.fire(
+                            "Error!",
+                            "Peserta belum absen masuk",
+                            "warning"
+                          );
+                        }
+                        // !CHANGEME: POST Requests absen keluar dan schema buat disable button
+                        console.log("ABSEN KELUAR");
+                        onClose();
+                      }}
+                      isDisabled={DummyPresensiData.state.keluar.isHadir}
+                    >
+                      Keluar
+                    </Button>
+                  </>
+                )}
+                <Spacer />
+                <Button color="#185C99" variant="outline" onClick={onClose}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </Box>
       </Layout>
     </>
