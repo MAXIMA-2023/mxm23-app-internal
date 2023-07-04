15f3c1ea37bf52758b9c5967f6dd127cd304fc78
rdhwan <fathanridhwann@gmail.com>
2023-07-05 00:07:05 +0700

    feat(layout): container dalam layout biar reusable
    - Buat container dalam layout dijadiin satu
    - Set title dari properti
    - Ada opsi buat button back to dashboard
    - Ada opsi buat disablePadding (buat container fullscreen buat QR scan)
    - Tambahin py for mobile (krn ada appBar sama bottomBar di design figma)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,86 @@
 import React from "react";
-import { Center, Box, Text, HStack } from "@chakra-ui/react";
+import { Center, Box, Text, HStack, Flex, Icon } from "@chakra-ui/react";
 import Sidebar from "./Sidebar";
+import Link from "next/link";
+import { BsArrowLeftShort } from "react-icons/bs";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+/* 
+  * property title : buat judul halaman
+  * property showDashboardButton :  kalo mau ada tombol back to dashboard (disable kalo buat dasboard)
+  * properti disablePadding :  kalo mau disable padding di content (buat halaman qr scan yg butuh fullscreen)
+
+  Content nya bisa dimasukkin ke children nya Layout
+  Liat contoh di dashboard yaa
+*/
+export default function Layout({
+  title,
+  showDashboardButton = false,
+  disablePadding = false,
+  children,
+}: {
+  title: string;
+  showDashboardButton?: boolean;
+  disablePadding?: boolean;
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Sidebar />
       <Box w={"full"} h={"full"} bgColor={"#FAFAFA"}>
-        {children}
+        <Box
+          w={"full"}
+          py={["1em", "1em", "1.5em", "3em", "3em"]}
+          px={["1em", "1em", "1.5em", "2em", "2em"]}
+        >
+          <Box
+            w={"full"}
+            py={["4em", "4em", "4em", "0em"]}
+            pl={["0", "0", "0", "17em", "20em"]}
+          >
+            <Box mb={"2em"}>
+              {showDashboardButton && (
+                <Link href={"/dashboard"}>
+                  <Flex
+                    alignItems={"center"}
+                    textColor="#9CA3AF"
+                    _hover={{ textColor: "#185C99" }}
+                    cursor={"pointer"}
+                    transition={"0.1s ease-in-out"}
+                  >
+                    <Icon as={BsArrowLeftShort} boxSize={6} />
+                    <Text ml={"0.7em"} fontSize={"md"}>
+                      Back to Dashboard
+                    </Text>
+                  </Flex>
+                </Link>
+              )}
+              <Text
+                fontSize={"4xl"}
+                fontWeight={"medium"}
+                textColor={"#1E1D22"}
+              >
+                {title}
+              </Text>
+            </Box>
+            <Flex
+              w={"full"}
+              minH={["95vh", "95vh", "auto", "86vh", "86vh"]}
+              bgColor={"white"}
+              borderRadius={"2xl"}
+              boxShadow={"lg"}
+            >
+              <Box w={"full"} p={disablePadding ? undefined : "2em"}>
+                <Flex justifyContent={"space-between"} alignItems={"center"}>
+                  {children}
+                </Flex>
+                {/* <Box
+                  maxH={["88.5%", "60vh", "75vh", "70vh", "70vh"]}
+                  overflowY={"auto"}
+                ></Box> */}
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
       </Box>
     </>
   );
