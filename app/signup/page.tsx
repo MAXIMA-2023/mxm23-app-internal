a9fb0cd799f2b3cd5468430fe8f95d6d8693fac9
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-07-14 15:53:21 +0700

    feat(signup): fixing forms & adding radio button

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Box, Text, Flex, Center, Stack, InputGroup, InputLeftElement, InputRightElement, Input, Icon, Button, Select, textDecoration, FormControl, FormLabel, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Flex,
+  Center,
+  Stack,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Input,
+  Icon,
+  Button,
+  Select,
+  textDecoration,
+  FormControl,
+  FormLabel,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Radio,
+  RadioGroup,
+  useRadio,
+  useRadioGroup,
+  HStack,
+} from "@chakra-ui/react";
 // import Container from "@/components/Container";
 import { RiAccountCircleFill } from "react-icons/ri";
 import { IoKey } from "react-icons/io5";
 import { BiShow, BiHide } from "react-icons/bi";
 import Link from "next/link";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 // import { useLocalStorage, useReadLocalStorage } from "usehooks-ts";
 import { isExpired } from "react-jwt";
 import axios from "axios";
@@ -19,13 +45,14 @@ interface IUserInfo {
   name: string;
   email: string;
   password: string;
-  divisiID: string;
   role: string;
+  divisiID: string;
   stateID: string;
 }
 
 export default function SignUpPanitia() {
   const [show, setShow] = React.useState(false);
+  const [role, setRole] = React.useState(0);
   const handleClick = () => setShow(!show);
 
   const router = useRouter();
@@ -33,11 +60,12 @@ export default function SignUpPanitia() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
+    control,
   } = useForm<IUserInfo>();
 
-  const [isButtonLoading, setIsButtonLoading] = useState(false);
-  const [error, setError] = useState(undefined);
+  // const [isButtonLoading, setIsButtonLoading] = useState(false);
+  // const [error, setError] = useState(undefined);
 
   // const [, setLocalStorage] = useLocalStorage("token", "");
   // const jwt = useReadLocalStorage("token");
@@ -49,36 +77,45 @@ export default function SignUpPanitia() {
   //   }
   // }, []);
 
+  // const onSubmit: SubmitHandler<IUserInfo> = async (data: IUserInfo) => {
+  //   try {
+  //     setIsButtonLoading(true);
+  //     const formData = new FormData();
+  //     formData.append("nim", data.nim);
+  //     formData.append("password", data.password);
+  //     formData.append("name", data.name);
+  //     formData.append("email", data.email);
+  //     formData.append("divisiID", data.divisiID);
+  //     formData.append("stateID", data.stateID);
+  //     // formData.append("role", data.role);
+  //     // await axios.post(`${process.env.API_URL}/api/`, formData);
+  //     console.log(data);
+  //     console.log("clicked");
+  //     toast.success("Berhasil Terdaftar! harap menunggu verifikasi dari BPH atau Facio", {
+  //       position: "bottom-left",
+  //       autoClose: 5000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //     });
+  //     setIsButtonLoading(false);
+  //     setTimeout(() => {
+  //       router.push("/signin");
+  //     }, 2000);
+  //   } catch (err: any) {
+  //     toast.error(err.response.data.message);
+  //     console.log(err.response.data.message);
+  //     setError(err.response.data.message);
+  //     setIsButtonLoading(false);
+  //   }
+  // };
+
+  //testing console log
   const onSubmit: SubmitHandler<IUserInfo> = async (data: IUserInfo) => {
-    try {
-      setIsButtonLoading(true);
-      const formData = new FormData();
-      formData.append("nim", data.nim);
-      formData.append("password", data.password);
-      formData.append("name", data.name);
-      formData.append("email", data.email);
-      formData.append("divisiID", data.divisiID);
-      formData.append("role", data.role);
-      await axios.post(`${process.env.API_URL}/api/`, formData);
-      toast.success("Berhasil Terdaftar! harap menunggu verifikasi dari BPH atau Facio", {
-        position: "bottom-left",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-      setIsButtonLoading(false);
-      setTimeout(() => {
-        router.push("/signin");
-      }, 2000);
-    } catch (err: any) {
-      toast.error(err.response.data.message);
-      console.log(err.response.data.message);
-      setError(err.response.data.message);
-      setIsButtonLoading(false);
-    }
+    console.log("clicked");
+    alert(JSON.stringify(data));
   };
 
   return (
@@ -100,7 +137,7 @@ export default function SignUpPanitia() {
                   </Text>
                 </Center>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                  <FormControl onSubmit={handleSubmit(onSubmit)}>
+                  <FormControl>
                     <Center mb={"1em"}>
                       <Stack w={"100%"} direction={"column"} spacing={"1em"}>
                         <Box>
@@ -132,7 +169,7 @@ export default function SignUpPanitia() {
                               borderRadius={"lg"}
                             />
                           </InputGroup>
-                          {errors.nim !== undefined && <Text textColor={"red"}>{errors.nim.message}</Text>}
+                          {errors.name !== undefined && <Text textColor={"red"}>{errors.name.message}</Text>}
                         </Box>
                         <Box>
                           <FormLabel>
@@ -143,7 +180,7 @@ export default function SignUpPanitia() {
                           <InputGroup>
                             <Input {...register("email", { required: "Email harus diisi" })} py={"1.25em"} type="email" placeholder="abc@student.umn.ac.id" size={"md"} borderRadius={"lg"} />
                           </InputGroup>
-                          {errors.nim !== undefined && <Text textColor={"red"}>{errors.nim.message}</Text>}
+                          {errors.email !== undefined && <Text textColor={"red"}>{errors.email.message}</Text>}
                         </Box>
                         <Box>
                           <FormLabel>
@@ -176,14 +213,37 @@ export default function SignUpPanitia() {
                               Role
                             </Text>
                           </FormLabel>
-                          <Tabs isFitted variant='enclosed'>
-                            <TabList mb='1em' {...register("role", { required: "Role harus dipilih" })}>
-                              <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Panitia</Tab>
-                              <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Organisator</Tab>
-                            </TabList>
-                            <TabPanels>
-                              <TabPanel>
-                              <Select {...register("divisiID", { required: "Divisi harus dipilih" })} w={"full"} placeholder={"Pilih Divisi"} size={"md"}>
+                          <Controller
+                            control={control}
+                            {...register("role", { required: "Role harus dipilih" })}
+                            render={({ field: { onChange, value } }) => (
+                              <>
+                                <RadioGroup onChange={onChange} value={value}>
+                                  <Stack direction="row">
+                                    <Box onClick={() => setRole(1)}>
+                                      <Radio value="1">Panitia</Radio>
+                                    </Box>
+                                    <Box onClick={() => setRole(2)}>
+                                      <Radio value="2">Organisator</Radio>
+                                    </Box>
+                                  </Stack>
+                                </RadioGroup>
+                                {errors.role !== undefined && <Text textColor={"red"}>{errors.role.message}</Text>}
+                              </>
+                            )}
+                          />
+                        </Box>
+                        {role === 0 ? (
+                          <></>
+                        ) : role === 1 ? (
+                          <>
+                            <Box>
+                              <FormLabel>
+                                <Text align={"left"} color={"black"} fontSize={"md"} fontWeight={"medium"}>
+                                  Pilh Divisi
+                                </Text>
+                              </FormLabel>
+                              <Select {...register("divisiID")} w={"full"} placeholder={"Pilih Divisi"} size={"md"}>
                                 <option value="1">Eventus - Acara</option>
                                 <option value="2">Novus - Publication</option>
                                 <option value="3">Effigia - Dokumentasi</option>
@@ -199,30 +259,44 @@ export default function SignUpPanitia() {
                                 <option value="13">Inspice - Registrasi</option>
                                 <option value="14">Armatura - Keamanan</option>
                               </Select>
-                              {errors.divisiID !== undefined && <Text textColor={"red"}>{errors.divisiID.message}</Text>}
-                              </TabPanel>
-                              <TabPanel>
-                              <Select {...register("stateID", { required: "State harus dipilih" })} w={"full"} size={"md"}>
-                                <option value="1">STATE</option>
+                            </Box>
+                          </>
+                        ) : role === 2 ? (
+                          <>
+                            <Box>
+                              <FormLabel>
+                                <Text align={"left"} color={"black"} fontSize={"md"} fontWeight={"medium"}>
+                                  Pilh STATE
+                                </Text>
+                              </FormLabel>
+                              <Select {...register("stateID")} w={"full"} placeholder={"Pilih STATE"} size={"md"}>
+                                <option value="1">Eventus - Acara</option>
+                                <option value="2">Novus - Publication</option>
+                                <option value="3">Effigia - Dokumentasi</option>
+                                <option value="4">Ornatus - Dekorasi</option>
+                                <option value="5">Facio - Website</option>
+                                <option value="6">Servanda - Sponsor</option>
+                                <option value="7">Mercatura - Merchandise</option>
+                                <option value="8">Nuntium - Media Relation</option>
+                                <option value="9">Lammina - Fresh Money</option>
+                                <option value="10">Fiducia - Perlengkapan</option>
+                                <option value="11">Emporium - Bazaar</option>
+                                <option value="12">Videre - Visual</option>
+                                <option value="13">Inspice - Registrasi</option>
+                                <option value="14">Armatura - Keamanan</option>
                               </Select>
-                              {errors.stateID !== undefined && <Text textColor={"red"}>{errors.stateID.message}</Text>}
-                              </TabPanel>
-                            </TabPanels>
-                          </Tabs>
-                        </Box>
+                            </Box>
+                          </>
+                        ) : (
+                          <></>
+                        )}
                       </Stack>
                     </Center>
                   </FormControl>
                   <Center mb={"1.5em"}>
-                    {isButtonLoading ? (
-                      <Button type={"submit"} w={"100%"} size={"md"} bgColor={"#185C99"} color={"white"} variant={"solid"} _hover={{ bgColor: "#185CDC" }} isLoading>
-                        Sign up
-                      </Button>
-                    ) : (
-                      <Button type={"submit"} w={"100%"} size={"md"} bgColor={"#185C99"} color={"white"} variant={"solid"} _hover={{ bgColor: "#185CDC" }}>
-                        Sign up
-                      </Button>
-                    )}
+                    <Button type={"submit"} w={"100%"} size={"md"} bgColor={"#185C99"} color={"white"} variant={"solid"} _hover={{ bgColor: "#185CDC" }} isLoading={isSubmitting}>
+                      Sign up
+                    </Button>
                   </Center>
                 </form>
                 <Box mb={"2em"}>
