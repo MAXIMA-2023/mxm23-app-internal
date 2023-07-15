260140143b2fa238ffc8cdf3d3f2f4b2466859fe
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-07-15 16:37:45 +0700

    feat: adding loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Box, Spinner } from "@chakra-ui/react";
+
+export default function Loading() {
+  return (
+    <Box position="fixed" top="0" left="0" right="0" bottom="0" zIndex="9999" bg="rgba(0,0,0,0.5)" display="flex" justifyContent="center" alignItems="center">
+      <Spinner size={"xl"} color={"#185C99"} />
+    </Box>
+  );
+}
